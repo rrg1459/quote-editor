16e7daf20827cbd886103db09e9eec71570dbef7
rafaDev <rrg1459@hotmail.com>
2025-01-19 09:12:35 -0400

    feat: making syntactic sugarr for broadcasts

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -6,9 +6,15 @@ class Quote < ApplicationRecord
   # these three lines are the same, they can be reduced by the following conventions
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
-  after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
 
-  after_update_commit -> { broadcast_replace_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Making broadcasting asynchronous with ActiveJob
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
 
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
