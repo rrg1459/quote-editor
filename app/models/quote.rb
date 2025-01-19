a87061b2479984bb967dc651717f2c0889fec615
rafaDev <rrg1459@hotmail.com>
2025-01-19 08:41:49 -0400

    feat: reducing lines by convention

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # these three lines are the same, they can be reduced by the following conventions
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+  after_create_commit -> { broadcast_prepend_to "quotes" }
 
 end
