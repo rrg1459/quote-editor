0f817f4a3ed0f17ed7f915f90c8899f1982ada88
rafaDev <rrg1459@hotmail.com>
2025-01-19 08:57:11 -0400

    feat: add broadcast_remove_to

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,6 +8,7 @@ class Quote < ApplicationRecord
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
   after_create_commit -> { broadcast_prepend_to "quotes" }
 
-  after_update_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 
 end
