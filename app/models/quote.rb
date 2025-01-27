a655193d4ac26e8cd544830f71f7309edc02c1e8
rafaDev <rrg1459@hotmail.com>
2025-01-27 09:30:27 -0400

    feat: add total_price to models

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,6 @@
 class Quote < ApplicationRecord
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
   belongs_to :company
 
   validates :name, presence: true
@@ -21,4 +22,8 @@ class Quote < ApplicationRecord
   # Those three callbacks are equivalent to the following single line
   # broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
